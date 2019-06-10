b22740ad18351f9dbd626001f78728a635fd8166
Andre Moura <andremarquesmoura@gmail.com>
2019-06-10 16:15:15 -0700

    add project file structure

---
+++ js/components/MapView/index.js
@@ -0,0 +1,5 @@
+import MapView from "./MapView";
+import MapViewContainer from "./MapViewContainer";
+
+export { MapView };
+export default MapViewContainer;
---
+++ js/screens/Reviews/index.js
@@ -0,0 +1,5 @@
+import Reviews from "./Reviews";
+import ReviewsContainer from "./ReviewsContainer";
+
+export { Reviews };
+export default ReviewsContainer;
