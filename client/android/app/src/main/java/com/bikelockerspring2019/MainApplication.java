dad8727d827806df0bfcc84e98710245f6c9f100
Jeremiah <admin@Jeremiahs-Mac-2.local>
2019-06-11 14:04:56 -0700

    added react-native-vector-icons dependency

--- client/android/app/src/main/java/com/bikelockerspring2019/MainApplication.java
+++ client/android/app/src/main/java/com/bikelockerspring2019/MainApplication.java
@@ -3,6 +3,7 @@ package com.bikelockerspring2019;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.arttitude360.reactnative.rngoogleplaces.RNGooglePlacesPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new RNGooglePlacesPackage(),
             new MapsPackage(),
             new RNGestureHandlerPackage()
