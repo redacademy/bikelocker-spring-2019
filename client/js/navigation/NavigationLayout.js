17a44bf4fdc24fa0ac4d50a4f726a9cffff31f86
Andre Moura <andremarquesmoura@gmail.com>
2019-06-11 14:33:40 -0700

    change file naming to use Locker instead of Spot

---
+++ client/js/navigation/NavigationLayout.js
@@ -0,0 +1,99 @@
+import React from "react";
+import Icon from "react-native-vector-icons/Ionicons";
+import {
+  createStackNavigator,
+  createBottomTabNavigator
+} from "react-navigation";
+import AboutScreen from "../screens/About";
+import ContactScreen from "../screens/Contact";
+import HowToUseAppScreen from "../screens/HowToUseApp";
+import ProfileScreen from "../screens/Profile";
+import MapScreen from "../screens/Map";
+import { sharedNavigationOptions } from "./config";
+import theme from "../config/styles";
+
+const MapStack = createStackNavigator(
+  {
+    Map: MapScreen
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      ...sharedNavigationOptions(navigation)
+    })
+  }
+);
+
+const AboutStack = createStackNavigator(
+  {
+    About: AboutScreen
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      ...sharedNavigationOptions(navigation)
+    })
+  }
+);
+const ContactStack = createStackNavigator(
+  {
+    Contact: ContactScreen,
+    HowToUseApp: HowToUseAppScreen
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      ...sharedNavigationOptions(navigation)
+    })
+  }
+);
+
+const ProfileStack = createStackNavigator(
+  {
+    Profile: ProfileScreen,
+    HowToUseApp: HowToUseAppScreen
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      ...sharedNavigationOptions(navigation)
+    })
+  }
+);
+
+export default createBottomTabNavigator(
+  {
+    Contact: ContactStack,
+    Map: MapStack,
+    Profile: ProfileStack,
+    About: AboutStack
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let IconComponent = Icon;
+        let iconName;
+        if (routeName === "Contact") {
+          iconName = `ios-calendar`;
+        } else if (routeName === "About") {
+          iconName = `ios-information-circle`;
+        } else if (routeName === "Profile") {
+          iconName = `ios-heart`;
+        } else if (routeName === "Map") {
+          iconName = `ios-map`;
+        }
+
+        // You can return any component that you like here!
+        return <IconComponent name={iconName} size={25} color={tintColor} />;
+      }
+    }),
+    tabBarOptions: {
+      activeTintColor: "white",
+      inactiveTintColor: "#999",
+      labelStyle: {
+        fontSize: 10,
+        fontFamily: theme.fontRegular
+      },
+      style: {
+        backgroundColor: "black"
+      }
+    }
+  }
+);
--- client/js/screens/Spot/index.js
+++
@@ -1,5 +0,0 @@
-import Spot from "./Spot";
-import SpotContainer from "./SpotContainer";
-
-export { Spot };
-export default SpotContainer;
