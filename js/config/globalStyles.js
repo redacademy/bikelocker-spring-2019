1436fec73e0f569d2137dfd5d9d805d332ac3d9c
Jeremiah <admin@Jeremiahs-Mac-2.local>
2019-06-11 10:41:24 -0700

    added global styling

---
+++ js/config/globalStyles.js
@@ -0,0 +1,23 @@
+const globalStyles = {
+  titleFont: "Avenir-Heavy", // Font Size should be 30px
+  subheadingFont: "Avenir-Light", // Font Size should be 20px
+  captionFont: "Open-Sans-Semibold", // Font Size should be 16px
+  bodyFont: "Open-Sans-Regular", //Font Size should be 16px
+  titeFontSize: 30, // For titles
+  subheadingFontSize: 20, // For subheadings
+  commonFontSize: 16, // For caption and body font sizes
+  mediumGreen: "#009887",
+  darkGreen: "005A4A",
+  orange: "#FCAF17",
+  darkGrey: "#404040",
+  lightGrey: "#DBDBDB",
+  white: "#FFF",
+  black: "#000",
+  mediumGrey: "#009887",
+  pinRed: "#FF0505",
+  pinYellow: "#FAE600",
+  pinTurquoise: "#0CF2D1",
+  pinGreen: "#479223"
+};
+
+export default globalStyles;
