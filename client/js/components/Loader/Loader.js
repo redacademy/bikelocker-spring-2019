a08f479e1dd18320a39708f3849e701428a4a4a9
Nancy Chu <nancy.q.chu@gmail.com>
2019-06-12 11:42:10 -0700

    create placeholder Loader component

---
+++ client/js/components/Loader/Loader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Loader = () => {
+  return (
+    <View>
+      <Text>This is the loader screen</Text>
+    </View>
+  );
+};
+
+export default Loader;
