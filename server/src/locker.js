969d2ce050185099186b914ee6a2c213f9589fc2
Gavin Jin <gavinjin62@gmail.com>
2019-06-12 14:04:14 -0700

    Add 50 converted addresses

--- server/src/locker.js
+++ server/src/locker.js
@@ -1,25 +1,9 @@
 const csvFilePath = "./BikeRackData.csv";
 const csvToJSON = require("csvtojson");
+const NodeGeocoder = require("node-geocoder");
 
 // have csv json, use new library, get 1 adress from csv,
 //use new lib to conver to lat long, once u get, put in array of object, export that new funtion
-
-// const NodeGeocoder = require("node-geocoder");
-
-const csvToJSONObject = async () => {
-  try {
-    const convertedCsv = await csvToJSON().fromFile(csvFilePath);
-    return convertedCsv;
-  } catch (error) {
-    return error;
-  }
-};
-
-let res = csvToJSONObject();
-
-res.then(e => {
-  console.log(e);
-});
 
 let options = {
   provider: "google",
@@ -28,15 +12,41 @@ let options = {
   formatter: null
 };
 
-// let geocoder = NodeGeocoder(options);
+let geocoder = NodeGeocoder(options);
 
-// geocoder
-//   .geocode(`${streetNumber} ${streetName}`)
-//   .then(function(res) {
-//     console.log(res);
-//   })
-//   .catch(function(err) {
-//     console.log(err);
-//   });
+const csvToJSONObject = async () => {
+  try {
+    const convertedCsv = await csvToJSON().fromFile(csvFilePath);
+
+    const randomSpots = [];
+    for (let index = 0; index < 50; index++) {
+      randomSpots.push(await convertedCsv[Math.floor(Math.random() * 1732)]);
+    }
+
+    randomSpots.map(async location => {
+      let address;
+      if (location && location["Street Name"] && location["Street Number"]) {
+        address = `${location["Street Name"]} ${location["Street Number"]}`;
+        if (address.includes(" St")) {
+          address.replace(" St", " Street");
+        }
+        if (address.includes(" Av")) {
+          address.replace(" Av", " Avenue");
+        }
+
+        try {
+          const parkingSpot = await geocoder.geocode(
+            address + ", Vancouver, BC, Canada"
+          );
+          console.log("RESPONSE", parkingSpot);
+        } catch (e) {
+          console.log(e.message);
+        }
+      }
+    });
+  } catch (error) {
+    return error;
+  }
+};
 
 module.exports = csvToJSONObject;
