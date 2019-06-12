e37a4b6b5bee65ac0f9d46060289a429f1d055d6
Jeremiah <admin@Jeremiahs-Mac-2.local>
2019-06-12 11:08:10 -0700

    mapView provider is google

--- client/js/components/MapView/MapView.js
+++ client/js/components/MapView/MapView.js
@@ -1,29 +1,36 @@
 import React, { Component } from "react";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
-// import { Marker } from "react-native-maps";
+import { Marker } from "react-native-maps";
 
 // import mapPin from "../../assets/images/map_pin.png";
 
-const MapViewComponent = () => {
-  return (
-    <MapView
-      provider={PROVIDER_GOOGLE}
-      style={{ flex: 1 }}
-      //   region={{
-      //     latitude: 49.263419,
-      //     longitude: -123.138192,
-      //     latitudeDelta: 0.0922,
-      //     longitudeDelta: 0.0421
-      //   }}
-    >
-      {/* <Marker
-        coordinate={{ latitude: 49.263419, longitude: -123.138192 }}
-        title={"Red Academy"}
-        image={mapPin}
-        tracksViewChanges={true}
-      /> */}
-    </MapView>
-  );
-};
+class MapViewComponent extends Component {
+  render() {
+    return (
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={{ flex: 1 }}
+        region={{
+          latitude: 49.263419,
+          longitude: -123.138192,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}
+        maxZoomLevel={20}
+        showsBuildings={true}
+        showsCompass={true}
+        showsUserLocation={true}
+        followsUserLocation={true}
+      >
+        <Marker
+          coordinate={{ latitude: 49.263419, longitude: -123.138192 }}
+          title={"Red Academy"}
+          // image={mapPin}
+          tracksViewChanges={true}
+        />
+      </MapView>
+    );
+  }
+}
 
 export default MapViewComponent;
