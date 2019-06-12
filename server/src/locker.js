7b218852eeb1ad6e3273afbd7658cf8f7a6e456a
Gavin Jin <gavinjin62@gmail.com>
2019-06-12 09:43:27 -0700

    add node geocoder

--- server/src/locker.js
+++ server/src/locker.js
@@ -1,12 +1,40 @@
 const csvFilePath = "./BikeRackData.csv";
 const csvToJSON = require("csvtojson");
 
+// have csv json, use new library, get 1 adress from csv,
+//use new lib to conver to lat long, once u get, put in object of array, export that new funtion
+
+// const NodeGeocoder = require("node-geocoder");
+
 const csvToJSONObject = async () => {
   try {
-    return await csvToJSON().fromFile(csvFilePath);
+    const convertedCsv = await csvToJSON().fromFile(csvFilePath);
+    return convertedCsv;
   } catch (error) {
     return error;
   }
 };
+
+console.log(csvToJSONObject);
+
+
+
+let options = {
+  provider: "google",
+  httpAdapter: "https",
+  apiKey: "AIzaSyAzi7L69BENzi4MiAH4751HBwOPWtAJS0Q",
+  formatter: null
+};
+
+// let geocoder = NodeGeocoder(options);
+
+// geocoder
+//   .geocode(`${streetNumber} ${streetName}`)
+//   .then(function(res) {
+//     console.log(res);
+//   })
+//   .catch(function(err) {
+//     console.log(err);
+//   });
 
 module.exports = csvToJSONObject;
