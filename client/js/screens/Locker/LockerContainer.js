6b13e4b1496ec88f7170355603ef83dd9b187471
Nancy Chu <nancy.q.chu@gmail.com>
2019-06-12 11:38:02 -0700

    write graphql query and pass query data as props to Locker

--- client/js/screens/Locker/LockerContainer.js
+++ client/js/screens/Locker/LockerContainer.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Locker from "./Locker";
+// import { Query } from "react-apollo";
+// import gql from "graphql-tag";
 
 export default class LockerContainer extends Component {
   constructor(props) {
@@ -8,6 +10,44 @@ export default class LockerContainer extends Component {
   }
 
   render() {
-    return <Locker />;
+    return;
+    //id variable will be passed in as prop later
+    <Query
+      query={GET_ONE_LOCKER_DATA}
+      variables={{ id: "cjwti3c164ynu0112ym882o5l" }}
+    >
+      {({ loading, data }) => {
+        if (loading || !data) return <Loader />;
+        return <Locker street={data.address} />;
+      }}
+    </Query>;
   }
 }
+
+// const GET_ONE_LOCKER_DATA = gql`
+//   query locker($id: ID!) {
+//     locker(id: $id) {
+//       id
+//       address
+//       latitude
+//       longitude
+//       reviews {
+//         id
+//         createdAt
+//         review
+//         rating
+//         reviewer {
+//           id
+//           lastName
+//           firstName
+//           email
+//         }
+//         photos {
+//           size
+//           id
+//           url
+//         }
+//       }
+//     }
+//   }
+// `;
