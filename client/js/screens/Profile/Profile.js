798977e98e71e82896dac419a95c3725af8783a5
Andre Moura <andremarquesmoura@gmail.com>
2019-06-14 13:10:00 -0700

    add content to Profile screen

--- client/js/screens/Profile/Profile.js
+++ client/js/screens/Profile/Profile.js
@@ -1,27 +1,32 @@
 import React from "react";
 import { Image, Text, TouchableOpacity, ScrollView, View } from "react-native";
 
-const Profile = ({ user }) => (
-  <ScrollView>
-    <View>
-      <Text>My Profile</Text>
-      <TouchableOpacity>
-        <Text>Edit</Text>
-      </TouchableOpacity>
-    </View>
-    <View>
-      <Image
-        style={{ width: 200, height: 200, borderRadius: 100 }}
-        source={user.profilePhoto}
-      />
-    </View>
-    <View>
-      <Text>Name</Text>
-      <Text>City</Text>
-      <Text>My bike is a...</Text>
-      <Text>Make and model of the bike</Text>
-    </View>
-  </ScrollView>
-);
+const Profile = ({ user }) => {
+  console.log("user", user);
+  return (
+    <ScrollView>
+      <View>
+        <Text>My Profile</Text>
+        <TouchableOpacity>
+          <Text>Edit</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <Image
+          style={{ width: 200, height: 200, borderRadius: 100 }}
+          source={{ uri: user.profilePhoto }}
+        />
+      </View>
+      <View>
+        <Text>
+          {user.firstName} {user.lastName}
+        </Text>
+        <Text>City</Text>
+        <Text>My bike is a...</Text>
+        <Text>Make and model of the bike</Text>
+      </View>
+    </ScrollView>
+  );
+};
 
 export default Profile;
--- client/js/screens/Profile/ProfileContainer.js
+++ client/js/screens/Profile/ProfileContainer.js
@@ -1,13 +1,6 @@
 import React, { Component } from "react";
 import Profile from "./Profile";
 
-const user = {
-  profilePhoto:
-    "https://media.licdn.com/dms/image/C4D03AQG0CbPDCBhKnQ/profile-displayphoto-shrink_200_200/0?e=1565827200&v=beta&t=Kt7MkNKHuqW-xyr6LBXxOrYArdO1DkZVtXNRVpwrzWQs",
-  firstName: "Andre",
-  lastName: "Moura"
-};
-
 export default class ProfileContainer extends Component {
   constructor(props) {
     super(props);
@@ -15,6 +8,13 @@ export default class ProfileContainer extends Component {
   }
 
   render() {
+    const user = {
+      profilePhoto:
+        "https://media.licdn.com/dms/image/C4D03AQG0CbPDCBhKnQ/profile-displayphoto-shrink_200_200/0?e=1565827200&v=beta&t=Kt7MkNKHuqW-xyr6LBXxOrYArdO1DkZVtXNRVpwrzWQs",
+      firstName: "Andre",
+      lastName: "Moura"
+    };
+
     return <Profile user={user} />;
   }
 }
