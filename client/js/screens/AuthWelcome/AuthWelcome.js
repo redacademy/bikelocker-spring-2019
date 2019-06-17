214ae732efb8b76d2ae5219b1510eef755bcb9af
Nancy Chu <nancy.q.chu@gmail.com>
2019-06-17 16:03:36 -0700

    change Authentication to a AuthWelcome component screen to include register and sign in buttons

--- client/js/screens/AuthWelcome/AuthWelcome.js
+++ client/js/screens/AuthWelcome/AuthWelcome.js
@@ -1,48 +1,47 @@
 import React from "react";
-import {
-  View,
-  Image,
-  Button,
-  ImageBackground,
-  Text,
-  TouchableOpacity
-} from "react-native";
-import styles from "./styles";
-// import { withNavigation } from "react-navigation";
+import { Text, AsyncStorage, View, Button } from "react-native";
 
 class AuthWelcome extends React.Component {
   static navigationOptions = {
-    title: "Welcome"
+    title: "SignIn"
   };
 
   render() {
     return (
-      <View style={styles.container}>
-        <ImageBackground
-          style={styles.backgroundImage}
-          source={require("../../assets/images/image.png")}
-        />
-        <Image
-          style={styles.logo}
-          source={require("../../assets/icons/bikelocker/combinedlogo/vertical/white.png")}
-        />
-        <View style={styles.btnRow}>
-          <TouchableOpacity
-            style={styles.button1}
-            onPress={() => this.props.navigation.navigate("Register")}
-          >
-            <Text style={styles.btnFont}> Register </Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.button2}
-            onPress={() => this.props.navigation.navigate("Login")}
-          >
-            <Text style={styles.btnFont}> Login </Text>
-          </TouchableOpacity>
-        </View>
+      <View>
+        <Button title="Sign in!" onPress={this._signInAsync} />
       </View>
     );
   }
+
+  _signInAsync = async () => {
+    await AsyncStorage.setItem("userToken", "abc");
+    this.props.navigation.navigate("FindLocker");
+  };
+}
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: "Welcome to the app!"
+  };
+
+  render() {
+    return (
+      <View>
+        <Button title="Show me more of the app" onPress={this._showMoreApp} />
+        <Button title="Actually, sign me out :)" onPress={this._signOutAsync} />
+      </View>
+    );
+  }
+
+  _showMoreApp = () => {
+    this.props.navigation.navigate("Other");
+  };
+
+  _signOutAsync = async () => {
+    await AsyncStorage.clear();
+    this.props.navigation.navigate("Auth");
+  };
 }
 
 export default AuthWelcome;
