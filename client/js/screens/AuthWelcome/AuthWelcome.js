dbd4e2a2d1b93bfbffd98a0730d8f43e6d8b0f0e
Nancy Chu <nancy.q.chu@gmail.com>
2019-06-17 16:09:17 -0700

    create authentication screen component for register and login screen

--- client/js/screens/AuthWelcome/AuthWelcome.js
+++ client/js/screens/AuthWelcome/AuthWelcome.js
@@ -9,39 +9,11 @@ class AuthWelcome extends React.Component {
   render() {
     return (
       <View>
-        <Button title="Sign in!" onPress={this._signInAsync} />
+        <Button title="Sign in!" />
+        <Button title="Register!" />
       </View>
     );
   }
-
-  _signInAsync = async () => {
-    await AsyncStorage.setItem("userToken", "abc");
-    this.props.navigation.navigate("FindLocker");
-  };
-}
-
-class HomeScreen extends React.Component {
-  static navigationOptions = {
-    title: "Welcome to the app!"
-  };
-
-  render() {
-    return (
-      <View>
-        <Button title="Show me more of the app" onPress={this._showMoreApp} />
-        <Button title="Actually, sign me out :)" onPress={this._signOutAsync} />
-      </View>
-    );
-  }
-
-  _showMoreApp = () => {
-    this.props.navigation.navigate("Other");
-  };
-
-  _signOutAsync = async () => {
-    await AsyncStorage.clear();
-    this.props.navigation.navigate("Auth");
-  };
 }
 
 export default AuthWelcome;
