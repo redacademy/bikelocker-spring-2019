79ac151c89de124f23f2a86e26ece01638762e20
Nancy Chu <nancy.q.chu@gmail.com>
2019-06-18 16:02:48 -0700

    add login button with asyn sign in function to route into FindLocker stack

---
+++ client/js/screens/Login/styles.js
@@ -0,0 +1,53 @@
+import { StyleSheet, Dimensions } from "react-native";
+import globalStyles from "../../config/globalStyles";
+
+const width = Dimensions.get("window").width;
+const height = Dimensions.get("window").height;
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center"
+  },
+  logo: {
+    width: width * 0.6,
+
+    marginTop: height * 0.1,
+    marginBottom: height * 0.1,
+    resizeMode: "contain"
+  },
+  btnRow: {
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  button1: {
+    flexDirection: "row",
+    height: 40,
+    width: "35%",
+    borderRadius: 20,
+    alignSelf: "center",
+    justifyContent: "space-around",
+    alignItems: "center",
+    backgroundColor: globalStyles.mediumGreen,
+    padding: 3,
+    margin: 10
+  },
+  btnFont: {
+    color: "white",
+    fontFamily: globalStyles.titleFont
+  },
+  labelText: {
+    padding: 10,
+    color: globalStyles.mediumGreen
+  },
+  form: {
+    borderStyle: "solid",
+    borderWidth: 2,
+    borderColor: globalStyles.lightGrey,
+    width: width * 0.8,
+    height: height * 0.06,
+    alignSelf: "center",
+    borderRadius: 15
+  }
+});
+
+export default styles;
--- client/js/screens/Register/Register.js
+++ client/js/screens/Register/Register.js
@@ -1,65 +1,25 @@
 import React, { Component } from "react";
-import { ActivityIndicator, StatusBar, View, Text } from "react-native";
+import { TouchableOpacity, View, Text, Image } from "react-native";
 import { Form, Field } from "react-final-form";
+import styles from "./styles";
 
 class Register extends Component {
   static navigationOptions = {
-    title: "Login"
+    title: "Register"
   };
   render() {
     return (
-      <View>
-        <Text>Register form</Text>
-        <Form
-          onSubmit={onSubmit}
-          validate={validate}
-          render={({ handleSubmit, pristine, invalid }) => (
-            <form onSubmit={handleSubmit}>
-              <h2>Simple Default Input</h2>
-              <div>
-                <label>First Name</label>
-                <Field
-                  name="firstName"
-                  component="input"
-                  placeholder="First Name"
-                />
-              </div>
-
-              <h2>An Arbitrary Reusable Input Component</h2>
-              <div>
-                <label>Interests</label>
-                <Field name="interests" component={InterestPicker} />
-              </div>
-
-              <h2>Render Function</h2>
-              <Field
-                name="bio"
-                render={({ input, meta }) => (
-                  <div>
-                    <label>Bio</label>
-                    <textarea {...input} />
-                    {meta.touched && meta.error && <span>{meta.error}</span>}
-                  </div>
-                )}
-              />
-
-              <h2>Render Function as Children</h2>
-              <Field name="phone">
-                {({ input, meta }) => (
-                  <div>
-                    <label>Phone</label>
-                    <input type="text" {...input} placeholder="Phone" />
-                    {meta.touched && meta.error && <span>{meta.error}</span>}
-                  </div>
-                )}
-              </Field>
-
-              <button type="submit" disabled={pristine || invalid}>
-                Submit
-              </button>
-            </form>
-          )}
+      <View style={styles.container}>
+        <Image
+          style={styles.logo}
+          source={require("../../assets/icons/bikelocker/combinedlogo/horizontal/black.png")}
         />
+        <TouchableOpacity
+          style={styles.button1}
+          onPress={() => this.props.navigation.navigate("Register")}
+        >
+          <Text style={styles.btnFont}> Register </Text>
+        </TouchableOpacity>
       </View>
     );
   }
