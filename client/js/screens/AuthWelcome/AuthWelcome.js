a1c02bbeb6964a00abb32cd54db5af8540e59d55
Nancy Chu <nancy.q.chu@gmail.com>
2019-06-18 13:46:19 -0700

    fix merge conflict

--- client/js/screens/AuthWelcome/AuthWelcome.js
+++ client/js/screens/AuthWelcome/AuthWelcome.js
@@ -1,16 +1,45 @@
 import React from "react";
-import { Text, AsyncStorage, View, Button } from "react-native";
+import {
+  View,
+  Image,
+  Button,
+  ImageBackground,
+  Text,
+  TouchableOpacity
+} from "react-native";
+import styles from "./styles";
+// import { withNavigation } from "react-navigation";
 
 class AuthWelcome extends React.Component {
   static navigationOptions = {
-    title: "SignIn"
+    title: "Welcome"
   };
 
   render() {
     return (
-      <View>
-        <Button title="Sign in!" />
-        <Button title="Register!" />
+      <View style={styles.container}>
+        <ImageBackground
+          style={styles.backgroundImage}
+          source={require("../../assets/images/image.png")}
+        />
+        <Image
+          style={styles.logo}
+          source={require("../../assets/icons/bikelocker/combinedlogo/vertical/white.png")}
+        />
+        <View style={styles.btnRow}>
+          <TouchableOpacity
+            style={styles.button1}
+            onPress={() => this.props.navigation.navigate("Register")}
+          >
+            <Text style={styles.btnFont}> Register </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button2}
+            onPress={() => this.props.navigation.navigate("Login")}
+          >
+            <Text style={styles.btnFont}> Login </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
