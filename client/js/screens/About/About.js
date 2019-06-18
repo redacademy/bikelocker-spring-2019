3ddbfd1a09ad1e563b18ba2343707dd53c43fdc8
jeremiahaguirre <happytobike@gmail.com>
2019-06-18 16:25:18 -0700

    removed locker modal from about

--- client/js/screens/About/About.js
+++ client/js/screens/About/About.js
@@ -1,18 +1,10 @@
 import React from "react";
 import { Text, View, Button } from "react-native";
-import { withNavigation } from "react-navigation";
 
 const About = ({ navigation }) => (
   <View>
     <Text>This is the About Screen</Text>
-    <Button
-      onPress={() => {
-        navigation.push("Locker");
-      }}
-      title="Locker Info"
-      color="#841584"
-    />
   </View>
 );
 
-export default withNavigation(About);
+export default About;
