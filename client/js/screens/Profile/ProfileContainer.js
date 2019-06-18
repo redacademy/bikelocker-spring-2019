acee4cd82239ea0d55a511a63ab814058d3951da
Andre Moura <andremarquesmoura@gmail.com>
2019-06-18 13:12:54 -0700

    add query to Profile page

--- client/js/screens/Profile/ProfileContainer.js
+++ client/js/screens/Profile/ProfileContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Text } from "react-native";
 import Profile from "./Profile";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
@@ -10,24 +11,37 @@ export default class ProfileContainer extends Component {
   }
 
   render() {
-    const user = {
-      firstName: "Andre",
-      lastName: "Moura",
-      city: "Vancouver",
-      province: "BC",
-      country: "Canada",
-      bikeColor: "Red",
-      bikeBrand: "Caloi",
-      bikeType: "Mountain Bike"
-    };
+    // const user = {
+    //   firstName: "Andre",
+    //   lastName: "Moura",
+    //   city: "Vancouver",
+    //   province: "BC",
+    //   country: "Canada",
+    //   bikeColor: "Red",
+    //   bikeBrand: "Caloi",
+    //   bikeType: "Mountain Bike"
+    // };
 
-    return <Profile user={user} />;
+    return (
+      <Query
+        query={USER_QUERY}
+        variables={{ id: "cjwtisae10ech0103e84u962r" }}
+        fetchPolicy="network-only"
+      >
+        {({ loading, error, data }) => {
+          console.log("data", data);
+          if (loading) return <Text>Loading...</Text>; // replace by Loader component
+          if (error) return <p>{`Error! ${error.message}`}</p>;
+          return <Profile user={data.User} />;
+        }}
+      </Query>
+    );
   }
 }
 
 const USER_QUERY = gql`
   query User($id: ID) {
-    User(id: "cjwtisae10ech0103e84u962r") {
+    User(id: $id) {
       profilePhoto {
         contentType
         id
