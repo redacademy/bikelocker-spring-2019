b48d049573c2318416dff6d35ce5ef07d1b8085b
Gavin Jin <gavinjin62@gmail.com>
2019-06-19 15:48:47 -0700

    add addlocker page styling

--- client/js/screens/AddLocker/styles.js
+++ client/js/screens/AddLocker/styles.js
@@ -1,26 +1,16 @@
 import { StyleSheet, Dimensions } from "react-native";
 
 const styles = StyleSheet.create({
+  allContainer: {
+    paddingTop: 5
+  },
   container: {
     width: "80%",
     alignSelf: "center"
   },
-  photoContainer: {
-    backgroundColor: "#009887",
-    height: Dimensions.get("window").height * 0.17,
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    paddingTop: 20
-  },
-  cameraIcon: {
-    color: "#fff",
-    fontSize: 40
-  },
-  cameraText: {
-    color: "#fff",
-    flex: 1,
-    justifyContent: "center"
+  addPhotos: {
+    flexDirection: "row",
+    justifyContent: "space-between"
   },
   address: {
     textAlign: "center",
