01982dffa4b61959ef0539cc45d41e2171582fa0
jeremiahaguirre <happytobike@gmail.com>
2019-06-20 13:58:37 -0700

    work on oboarding screens

--- client/js/screens/Contact/Contact.js
+++ client/js/screens/Contact/Contact.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Text, View } from "react-native";
+import OnboardingContainer from "../Onboarding";
 
 const Contact = ({ params }) => (
-  <View>
-    <Text>This is the Contact Us Screen</Text>
+  <View style={{ flex: 1, height: "100%", width: "100%" }}>
+    {/* <Text>This is tact Us Screen</Text> */}
+    <OnboardingContainer />
   </View>
 );
 
--- client/js/screens/Onboarding/Onboarding.js
+++ client/js/screens/Onboarding/Onboarding.js
@@ -1,6 +1,43 @@
 import React from "react";
+import { View, TouchableOpacity } from "react-native";
 import styles from "./styles";
+import Onboarding from "react-native-onboarding-swiper";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-const Onboarding = () => {};
+const Onboarding = () => {
+  const onboardingScreens = [
+    {
+      backgroundColor: "#fff",
+      title: "Onboarding",
+      subtitle: "Done with React Native Onboarding Swiper"
+    },
+    {
+      backgroundColor: "#fe6e58",
+      title: "The Title",
+      subtitle: "This is the subtitle that sumplements the title."
+    },
+    {
+      backgroundColor: "#999",
+      title: "Triangle",
+      subtitle: "Beautiful, isn't it?"
+    }
+  ];
+  return (
+    <View style={{ flex: 1, height: "100%", width: "100%" }}>
+      <TouchableOpacity style={{ backgroundColor: "black" }}>
+        <Onboarding
+          onSkip={() => console.log("this will go to the map screen")}
+          showSkip
+          nextLabel
+          controlStatusBar
+          showPagination
+          transitionAnimationDuration
+          showNext
+          pages={onboardingScreens}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 export default Onboarding;
