200371e42ae7af487cb22bf239105c55836fedd3
Nancy Chu <nancy.q.chu@gmail.com>
2019-06-19 17:19:07 -0700

    add updateLocker mutation to set avgRating in Locker based on average.

--- server/src/avgRating.js
+++ server/src/avgRating.js
@@ -1,51 +1,30 @@
-// export default event => {
-//   console.log(event.data);
-//   const average =
-//     event.data.reviews.reduce((a, b) => a.rating + b.rating, 0) /
-//     event.data.reviews.length;
+const { fromEvent } = require("graphcool-lib");
 
-//   event.data.avgRating = average;
-
-//   console.log(event.data);
-
-//   return event;
-// };
-
-export default async event => {
+module.exports = event => {
+  //   const average =
+  //     event.data.reviews.reduce((a, b) => a.rating + b.rating, 0) /
+  //     event.data.reviews.length;
   console.log(event);
+  const lockerId = event.data.id;
+  const avgRating = 3;
 
-  try {
-    const graphcool = fromEvent(event);
-    const api = graphcool.api("simple/v1");
-    const lockerId = event.data.id;
-    const avgRating = 5;
-    //update
-    const updateLocker = await updateLockerAvgRating(lockerId, avgRating);
+  // Create Graphcool API (based on https://github.com/graphcool/graphql-request)
+  const graphcool = fromEvent(event);
+  const api = graphcool.api("simple/v1");
 
-    return event;
-  } catch (e) {
-    console.log(e);
-    return { error: "An unexpected error. " };
-  }
-};
+  // Create variables for mutation
+  const variables = { id: lockerId, avgRating: avgRating };
 
-async function updateLockerAvgRating(id, avgRating) {
-  const mutation = `
-      mutation updateLocker($id: ID!, $avgRating: Int) {
-        updateLocker(
-          id: $id
-          avgRating: $avgRating,
-        ) {
-          id
-          avgRating
-        }
+  // Create mutation
+  const updateLockerMutation = `
+    mutation locker($id: ID!, $avgRating: Int) {
+      updateLocker(id: $id, avgRating: $avgRating) {
+        id
+        avgRating
       }
-    `;
+    }
+  `;
 
-  const variables = {
-    id: id,
-    avgRating: avgRating
-  };
-
-  return api.request < { updateLocker: Locker } > (mutation, variables);
-}
+  // Send mutation with variables
+  return api.request(updateLockerMutation, variables);
+};
