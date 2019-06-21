37546f7ffaa63e4b3f7ba3891b1aa08aba12b958
Nancy Chu <nancy.q.chu@gmail.com>
2019-06-21 09:47:02 -0700

    delete unused resolvers and hooks

--- server/src/startRating.js
+++
@@ -1,8 +0,0 @@
-// console.log("heyo");
-// updateOrCreateReview(update: UpdateReview!, create: CreateReview!): Review
-export default event => {
-  console.log(event.data);
-  event.data.avgRating = 1;
-
-  return event;
-};
