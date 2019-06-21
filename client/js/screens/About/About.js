2db9a74ed976c8a26d970408dacef90dc8dd3ea7
Gavin Jin <gavinjin62@gmail.com>
2019-06-20 23:38:20 -0700

    remove add locker bav from about.js

--- client/js/screens/About/About.js
+++ client/js/screens/About/About.js
@@ -1,10 +1,20 @@
 import React from "react";
-import { Text, View, TouchableOpacity } from "react-native";
+import { Text, View, Image } from "react-native";
+import styles from "./styles";
 
-const About = ({ navigation }) => (
-  <TouchableOpacity onPress={() => navigation.navigate("AddLocker", {})}>
-    <Text>This is the About Screen</Text>
-  </TouchableOpacity>
+const About = () => (
+  <View style={styles.container}>
+    <Text style={styles.h2}>About Bike Locker</Text>
+    <Text style={styles.p}>
+      Bike Locker is an app that enables cyclists to find safe parking for their
+      bicycle in Vancouver. Using the app, users can locate an available space,
+      map their route, and rate and review the space.
+    </Text>
+    <Image
+      source={require("../../assets/icons/bikelocker/combinedlogo/vertical/mediumgreen.png")}
+      style={styles.image}
+    />
+  </View>
 );
 
 export default About;
