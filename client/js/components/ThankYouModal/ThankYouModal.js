e62d5545550d41c055e677248bb7fb7f63cc3cb3
Gavin Jin <gavinjin62@gmail.com>
2019-06-24 13:27:10 -0700

    add modal styling

--- client/js/components/ThankYouModal/ThankYouModal.js
+++ client/js/components/ThankYouModal/ThankYouModal.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
-import { Button, Text, View } from "react-native";
+import { Button, Text, View, TouchableOpacity } from "react-native";
 import Modal from "react-native-modal";
+import styles from "./styles";
 
 const ThankYouModal = ({ toggleModal, state }) => {
   return (
     <View style={{ flex: 1 }}>
       <Modal isVisible={state.isModalVisible}>
-        <View style={{ flex: 1 }}>
-          <Text>Hello!</Text>
-          <Button title="Hide modal" onPress={toggleModal} />
+        <View style={styles.container}>
+          <Text style={styles.thankYou}>Thank you!</Text>
+          <Text style={styles.modalText}>
+            You’re helping to reduce bike theft in your city and making the
+            Biking Community better!
+          </Text>
+          <TouchableOpacity onPress={toggleModal} style={styles.continueContainer}>
+            <Text style={styles.continue}>Continue</Text>
+          </TouchableOpacity>
         </View>
       </Modal>
     </View>
