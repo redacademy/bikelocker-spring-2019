3a0c76c16d61b283e15d52dc7bd67d04c4cb11f1
Nancy Chu <nancy.q.chu@gmail.com>
2019-06-24 09:49:59 -0700

    add signin mutation

--- client/js/screens/Login/LoginContainer.js
+++ client/js/screens/Login/LoginContainer.js
@@ -1,11 +1,35 @@
 import React, { Component } from "react";
 import { Text } from "react-native";
 import Login from "./Login";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
+import Loader from "../../components/Loader";
 
 class LoginContainer extends Component {
   render() {
-    return <Login navigation={this.props.navigation} />;
+    return (
+      <Mutation mutation={AUTHENTICATE_USER}>
+        {(authenticateUser, { loading, error }) => {
+          if (loading) return <Loader />;
+          return (
+            <Login
+              navigation={this.props.navigation}
+              authenticate={authenticateUser}
+            />
+          );
+        }}
+      </Mutation>
+    );
   }
 }
+
+const AUTHENTICATE_USER = gql`
+  mutation signIn($email: String!, $password: String!) {
+    authenticateUser(email: $email, password: $password) {
+      id
+      token
+    }
+  }
+`;
 
 export default LoginContainer;
