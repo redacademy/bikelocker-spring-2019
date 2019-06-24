453824414a87384e6f05f21305d232503976b7bc
jeremiahaguirre <happytobike@gmail.com>
2019-06-24 15:08:24 -0700

    contact screen

--- client/js/screens/Contact/Contact.js
+++ client/js/screens/Contact/Contact.js
@@ -1,26 +1,69 @@
-import React from "react";
+import React, { Component } from "react";
 import { Text, View, TouchableOpacity, TextInput } from "react-native";
+import theme from "../../config/globalStyles";
+import Modal from "react-native-modal";
+import styles from "./styles";
 
-const Contact = ({ params }) => (
-  <View>
-    <Text>Contact Us</Text>
-    <Text>We'd love to hear from you! Thoughts, comments, feedback.</Text>
-    <TextInput
-      autoCapitalize
-      autoCompleteType
-      autoCorrect
-      {...this.props} // Inherit any props passed to it; e.g., multiline, numberOfLines below
-      editable={true}
-      maxLength={40}
-      placeholder={"Leave your comment here..."}
-      style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-      // onChangeText={text => this.setState({ text })}
-      // value={this.state.text}
-    />
-    <TouchableOpacity>
-      <Text>Submit</Text>
-    </TouchableOpacity>
-  </View>
-);
+class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: null,
+      isModalVisible: false
+    };
+  }
+
+  toggleModal = () => {
+    if (this.state.isModalVisible === false) {
+      this.setState({ isModalVisible: true });
+    } else if (this.state.isModalVisible === true) {
+      this.setState({ isModalVisible: false });
+    }
+  };
+  render() {
+    const { isModalVisible } = this.state;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.h2}>Contact Us</Text>
+        <Text style={styles.p}>
+          We'd love to hear from you! Thoughts, comments, feedback.
+        </Text>
+        <Text style={styles.comment}>Comment</Text>
+        <TextInput
+          {...this.props}
+          autoCapitalize={"sentences"}
+          autoCorrect={true}
+          maxLength={200}
+          placeholder={"Leave your comment here..."}
+          placeholderTextColor={"black"}
+          style={styles.textInput}
+          onChangeText={text => this.setState({ text })}
+          value={this.state.text}
+          multiline={true}
+        />
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => {
+            console.log(isModalVisible);
+            this.setState({ isModalVisible: !isModalVisible });
+          }}
+        >
+          <Text style={styles.btnText}>Submit</Text>
+        </TouchableOpacity>
+        {isModalVisible === true ? (
+          <Modal isVisible={isModalVisible}>
+            <View style={styles.modalView}>
+              <Text style={styles.thanks}>Thank you!</Text>
+              <Text style={styles.subHeading}>Thanks for the feedback!</Text>
+              <TouchableOpacity style={styles.btn2} onPress={this.toggleModal}>
+                <Text style={styles.btnText}>Continue</Text>
+              </TouchableOpacity>
+            </View>
+          </Modal>
+        ) : null}
+      </View>
+    );
+  }
+}
 
 export default Contact;
