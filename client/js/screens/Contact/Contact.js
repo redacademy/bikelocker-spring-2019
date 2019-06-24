4e438860eb013339f3035152055facca341765de
jeremiahaguirre <happytobike@gmail.com>
2019-06-24 10:37:27 -0700

    remove commented code and move styles to styles.js

--- client/js/screens/Contact/Contact.js
+++ client/js/screens/Contact/Contact.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Text, View } from "react-native";
 import OnboardingContainer from "../Onboarding";
+import styles from "./styles";
 
 const Contact = ({ params }) => (
-  <View style={{ flex: 1, height: "100%", width: "100%" }}>
-    {/* <Text>This is tact Us Screen</Text> */}
+  <View style={styles.container}>
     <OnboardingContainer />
   </View>
 );
