ed9e5658e5100c28769d56be5aa9f18644116004
jeremiahaguirre <happytobike@gmail.com>
2019-06-24 11:45:42 -0700

    layout for contact screen

--- client/js/screens/Contact/Contact.js
+++ client/js/screens/Contact/Contact.js
@@ -1,9 +1,25 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity, TextInput } from "react-native";
 
 const Contact = ({ params }) => (
   <View>
-    <Text>This is the Contact Us Screen</Text>
+    <Text>Contact Us</Text>
+    <Text>We'd love to hear from you! Thoughts, comments, feedback.</Text>
+    <TextInput
+      autoCapitalize
+      autoCompleteType
+      autoCorrect
+      {...this.props} // Inherit any props passed to it; e.g., multiline, numberOfLines below
+      editable={true}
+      maxLength={40}
+      placeholder={"Leave your comment here..."}
+      style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+      // onChangeText={text => this.setState({ text })}
+      // value={this.state.text}
+    />
+    <TouchableOpacity>
+      <Text>Submit</Text>
+    </TouchableOpacity>
   </View>
 );
 
