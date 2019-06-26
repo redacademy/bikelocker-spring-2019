ceb64de9c885f1acf29e8bb2c97407d942118298
Andre Moura <andremarquesmoura@gmail.com>
2019-06-26 14:00:16 -0700

    add logout to DrawerItems in the Drawer Menu

--- client/js/navigation/NavigationLayout.js
+++ client/js/navigation/NavigationLayout.js
@@ -22,70 +22,83 @@ import AddLockerScreen from "../screens/AddLocker";
 const FindLockerStack = createStackNavigator(
   {
     FindLocker: FindLockerScreen,
-    AddLocker: AddLockerScreen,
+    AddLocker: AddLockerScreen
   },
   {
     defaultNavigationOptions: ({ navigation }) => ({
-      ...sharedNavigationOptions(navigation),
-    }),
-  },
+      ...sharedNavigationOptions(navigation)
+    })
+  }
 );
 
 const ProfileStack = createStackNavigator(
   {
     Profile: ProfileScreen,
-    EditProfile: EditProfileScreen,
+    EditProfile: EditProfileScreen
   },
   {
     defaultNavigationOptions: ({ navigation }) => ({
-      ...sharedNavigationOptions(navigation),
-    }),
-  },
+      ...sharedNavigationOptions(navigation)
+    })
+  }
 );
 
 const AboutStack = createStackNavigator(
   {
-    About: AboutScreen,
+    About: AboutScreen
   },
   {
     defaultNavigationOptions: ({ navigation }) => ({
-      ...sharedNavigationOptions(navigation),
-    }),
-  },
+      ...sharedNavigationOptions(navigation)
+    })
+  }
 );
 
 const ContactStack = createStackNavigator(
   {
-    Contact: ContactScreen,
+    Contact: ContactScreen
   },
   {
     defaultNavigationOptions: ({ navigation }) => ({
-      ...sharedNavigationOptions(navigation),
-    }),
-  },
+      ...sharedNavigationOptions(navigation)
+    })
+  }
 );
 
 const HowToUseAppStack = createStackNavigator(
   {
-    HowToUseApp: HowToUseAppScreen,
+    HowToUseApp: HowToUseAppScreen
   },
   {
     defaultNavigationOptions: ({ navigation }) => ({
-      ...sharedNavigationOptions(navigation),
-    }),
-  },
+      ...sharedNavigationOptions(navigation)
+    })
+  }
 );
 
 const ResourcesStack = createStackNavigator(
   {
-    Resources: ResourcesScreen,
+    Resources: ResourcesScreen
   },
   {
     defaultNavigationOptions: ({ navigation }) => ({
-      ...sharedNavigationOptions(navigation),
-    }),
-  },
+      ...sharedNavigationOptions(navigation)
+    })
+  }
 );
+
+const Logout = () => {
+  return null;
+};
+
+logout = async props => {
+  try {
+    await clearStorage();
+    props.navigation.navigate("AuthLoading");
+  } catch (e) {
+    throw e;
+  }
+};
 
 const DrawerContent = props => (
   <View>
@@ -93,10 +106,10 @@ const DrawerContent = props => (
       style={{
         backgroundColor: theme.mediumGreen,
         height: 90,
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between"
       }}
     >
       <TouchableOpacity onPress={() => props.navigation.closeDrawer()}>
@@ -108,14 +121,25 @@ const DrawerContent = props => (
         />
       </TouchableOpacity>
       <Image
-        source={require('../assets/images/bikelockerlogo-white.png')}
+        source={require("../assets/images/bikelockerlogo-white.png")}
         style={{ width: 40, height: 40, marginTop: 10 }}
       />
       <View style={{ width: 40, marginRight: 10 }} />
     </View>
-    <DrawerItems {...props} />
+    <DrawerItems
+      {...props}
+      onItemPress={navigation => {
+        if (navigation.route.routeName === "Logout") {
+          this.logout(props);
+        } else {
+          props.onItemPress(navigation);
+          return;
+        }
+      }}
+    />
   </View>
 );
+
 export default createDrawerNavigator(
   {
     FindLocker: {
@@ -145,52 +169,39 @@ export default createDrawerNavigator(
     HowToUseApp: {
       screen: HowToUseAppStack,
       navigationOptions: ({ navigation }) => ({
-        title: 'How to Use the App',
-      }),
+        title: "How to Use the App"
+      })
     },
     Resources: {
       screen: ResourcesStack,
       navigationOptions: ({ navigation }) => ({
-        title: 'Helpful Resources',
-      }),
+        title: "Helpful Resources"
+      })
     },
+    Logout: Logout
   },
   {
     contentComponent: props => (
       <View style={{ flex: 1 }}>
         <DrawerContent {...props} />
-        <TouchableOpacity
-          onPress={async () => {
-            try {
-              await clearStorage();
-              props.navigation.navigate("AuthLoading");
-            } catch (e) {
-              throw e;
-            }
-          }}
-        >
-          <Text style={{ fontSize: 16, marginLeft: 15 }} title="Logout">
-            Logout
-          </Text>
-        </TouchableOpacity>
       </View>
     ),
     overlayColor: "black"
   },
   {
     defaultNavigationOptions: ({ navigation }) => ({}),
-    drawerWidth: '80%',
+    drawerWidth: "80%",
     contentOptions: {
       activeTintColor: theme.mediumGreen,
       labelStyle: {
-        fontSize: 16,
+        fontSize: 16
       },
       itemsContainerStyle: {
-        margin: 0,
+        margin: 0
       },
       iconContainerStyle: {
-        margin: 0,
-      },
-    },
-  },
+        margin: 0
+      }
+    }
+  }
 );
